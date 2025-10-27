5be27c6cd5039fc201ebd8e7d580806708e1f4ce
Yuria Loo <yuria.loo@comcast.net>
2025-10-26 17:52:42 -0700

    Add contents from data

--- my-react-app/src/components/Recipe.jsx
+++ my-react-app/src/components/Recipe.jsx
@@ -3,6 +3,24 @@ const Recipe = (props) => {
     return (
         <>
             <div className="recipe">
+                <h2 className="recipe-name">{props.recipe.name}</h2>
+                <h4 className="section-name">Ingredients</h4>
+                <ul className="ingredient-list">
+                    {props.recipe.ingredients.map(ingredient => 
+                        <li>
+                            {ingredient}
+                        </li>
+                    )}
+                </ul>
+
+                <h4 className="section-name">Instructions</h4>
+                <ol className="instruction-list">
+                    {props.recipe.instructions.map(instruction => 
+                        <li>
+                            {instruction}
+                        </li>
+                    )}
+                </ol>
 
             </div>
         </>
--- my-react-app/src/components/RecipeContainer.jsx
+++ my-react-app/src/components/RecipeContainer.jsx
@@ -1,10 +1,11 @@
-import { recipes } from '../data/data.js'
+import { recipes } from '../assets/data.js'
 import Recipe from './Recipe.jsx'
 
 const RecipeContainer = () => {
     return (
         <>
             <div className="recipeContainer">
+                <h1>Bread Recipes</h1>
                 {recipes.map(recipe => <Recipe key={recipe.name} recipe={recipe} />)}
             </div>
         </>
